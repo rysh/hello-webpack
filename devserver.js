227436d5d22e87971fb65007d94ec653930b0e83
rysh <rysh@rysh.local>
2017-01-02 07:55:24 +0900

    Proxyを使う
    $node devserver.js
    $node appserver.js　で、実行。
    http://localhost:3000　で、確認。

--- devserver.js
+++ devserver.js
@@ -2,9 +2,21 @@ var WebpackDevServer = require("webpack-dev-server");
 var webpack = require("webpack");
 var config = require("./webpack.config.js");
 
+Object.keys(config.entry).forEach(function (key) {
+  config.entry[key].unshift(
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server'
+  );
+});
+
+config.plugins = [
+  new webpack.HotModuleReplacementPlugin(), 
+];
+
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler, {
     publicPath: config.output.publicPath,
+    hot: true,
+    proxy: { '*': 'http://localhost:3000' } // proxyの設定
 });
 server.listen(8080);
-
